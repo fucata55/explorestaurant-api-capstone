70ee79e5120eb412f4ea736bf0899832f33c6f9f
fucata <sliong325@gmail.com>
2017-10-03 21:47:52 -0500

    completing UX and CSS

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //Step 1. Get user input
 //Can input type text be submitted by "enter" button, without having "submit" button
-$('.input-box').on('click', function (event) {
+$('.search-query').submit(function (event) {
     event.preventDefault();
-    let userInput = $('input').val();
+    let userInput = $('.input-box').val();
     console.log(userInput);
     getZomatoLocation(userInput);
     //    getZomatoResults(userInput);
-    getYoutubeResults(userInput);
+    getYoutubeResults('best food in ' + userInput);
+    $('main').show();
+    $('.input-box').val('')
 })
 
 //step 2. Get JSON response from zomato
@@ -89,7 +91,7 @@ function getYoutubeResults(userSearchTerm) {
         key: 'AIzaSyAuQxnyeTiG2xHqYKu3EDRxP9owZqfiAjc',
         maxResults: 10,
         type: 'video',
-    }, displayYoutubeResults())
+    }, displayYoutubeResults)
     //    console.log('getYoutubeResults ran')
 }
 
@@ -98,32 +100,89 @@ function displayZomatoResults(apiResponse) {
     $('.zomato-outputs').empty();
     for (let i = 0; i < apiResponse.best_rated_restaurant.length; i++) {
         let restaurantName = apiResponse.best_rated_restaurant[i].restaurant.name;
-        let restaurantImage = apiResponse.best_rated_restaurant[i].restaurant.featured_image;
-        let restaurantStreet = apiResponse.best_rated_restaurant[i].restaurant.location.address;
-        let restaurantCity = apiResponse.best_rated_restaurant[i].restaurant.location.locality_verbose;
+        let restaurantCurrency = apiResponse.best_rated_restaurant[i].restaurant.currency;
+        let restaurantAverageCost = apiResponse.best_rated_restaurant[i].restaurant.average_cost_for_two
+        let restaurantPage = apiResponse.best_rated_restaurant[i].restaurant.events_url
+        let restaurantCuisine = apiResponse.best_rated_restaurant[i].restaurant.cuisines;
+        let restaurantImage = apiResponse.best_rated_restaurant[i].restaurant.featured_image
         //        console.log(restaurantName, restaurantImage, restaurantStreet, restaurantCity);
-        $('.zomato-outputs').append(`
-            <div class='zomato-output'>
-            <img src="${restaurantImage}" class='zomato-output-image' alt="">
-            <h3 class='zomato-output-restaurant-name'>${restaurantName}</h3>
-            <p class='zomato-output-restaurant-street'>${restaurantStreet}</p>
-            <p class='zomato-output-restaurant-state'>${restaurantCity}</p>
-            </div>`)
-    }
 
+        function b(image) {
+            console.log('function b ran')
+            if (image == "") {
+                let restaurantImage = 'https://i.pinimg.com/736x/42/c3/39/42c3397907bbcbded0b912fa956f8a42--scallop-plating-fish-plating.jpg';
+                $('.zomato-outputs').append(`
+<div class='zomato-output'>
+<a href=${restaurantPage} class='zomato-output-image' target='_blank'>
+<div style='background-image: url(${restaurantImage})' class='zomato-output-image'></div>
+</a>
+<div class='restaurant-detail'>
+<h3 class='zomato-output-restaurant-name'>${restaurantName}</h3>
+<p class='zomato-output-restaurant-street'>Average cost for two: ${restaurantCurrency}${restaurantAverageCost}</p>
+<p class='zomato-output-restaurant-state'>${restaurantCuisine}</p>
+<div>
+</div>`)
+            } else {
+                let restaurantImage = apiResponse.best_rated_restaurant[i].restaurant.featured_image;
+                $('.zomato-outputs').append(`
+<div class='zomato-output'>
+<a href=${restaurantPage} class='zomato-output-image' target='_blank'>
+<div style='background-image: url(${restaurantImage})' class='zomato-output-image'></div>
+</a>
+<div class='restaurant-detail'>
+<h3 class='zomato-output-restaurant-name'>${restaurantName}</h3>
+<p class='zomato-output-restaurant-street'>Average cost for two: ${restaurantCurrency} ${restaurantAverageCost}</p>
+<p class='zomato-output-restaurant-state'>${restaurantCuisine}</p>
+<div>
+</div>`)
+            }
+        };
+        b(restaurantImage);
+
+
+    }
 }
 
 function displayYoutubeResults(results) {
     console.log('displayYoutubbeResults() ran')
     $('.youtube-outputs').empty();
-    let videoImage = results.snippet.thumbnails.high.url;
-    let videoName = results.snippet.title;
-    let videoLink = results.id.videoId
-    $('.youtube-outputs').append(`
+    console.log(results);
+    for (let i = 0; i < 10; i++) {
+        let videoImage = results.items[i].snippet.thumbnails.default.url;
+        let videoName = results.items[i].snippet.title;
+        let videoLink = results.items[i].id.videoId;
+        $('.youtube-outputs').append(`
     <div class='youtube-output'>
-    <a href='https://www.youtube.com/watch?v=" + ${videoLink} + "' target='_blank'>"
-    <img src="${videoImage}" class='youtube-output-image' alt="">
+    <a href='https://www.youtube.com/watch?v=${videoLink}' class ='link' target='_blank'>
+    <div style='background-image: url(${videoImage})' class='youtube-output-image'></div>
     </a>
-    <h3 class='zomato-output-restaurant-name'>${videoName}</h3>
+    <div class='video-detail'>
+    <h3 class='youtube-output-video-name'>${videoName}</h3>
+    </div>
     </div>`)
+    };
 }
+
+function a() {
+    $('main').hide();
+}
+
+
+$(document).ready(function () {
+    let placeHolder = ['Search by Location', 'Country', 'State', 'City', 'Area'];
+    let n = 0;
+    let loopLength = placeHolder.length;
+
+    setInterval(function () {
+        if (n < loopLength) {
+            var newPlaceholder = placeHolder[n];
+            n++;
+            $('input').attr('placeholder', newPlaceholder);
+        } else {
+            $('input').attr('placeholder', placeHolder[0]);
+            n = 0;
+        }
+    }, 2500);
+});
+
+a()
